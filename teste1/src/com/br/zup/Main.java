6621e13864e06badbeaabccb4bec84180217f948
Kaio <kaio.moraes@zup.com.br>
2021-05-25 10:01:52 -0300

    atribuir idade

---
+++ teste1/src/com/br/zup/Main.java
@@ -0,0 +1,20 @@
+package com.br.zup;
+
+/**
+ * Capitulo 3 - 25/05
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// imprimir a idade
+        int idade = 33;
+        System.out.println(idade);
+
+        // somar a idade no ano seguinte
+        int idadeNoAnoSeguinte;
+        idadeNoAnoSeguinte = idade + 1;
+
+        //imprime a idade
+        System.out.println(idadeNoAnoSeguinte);
+    }
+}
