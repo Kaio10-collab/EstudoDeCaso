2ea0f218565528dcbf3f8a81b216de6f502f9065
Kaio <kaio.moraes@zup.com.br>
2021-05-25 13:27:37 -0300

    Imprimir multiplo de 3.

---
+++ teste1/src/com/br/zup/ImprimeMultiplo.java
@@ -0,0 +1,15 @@
+package com.br.zup;
+
+public class ImprimeMultiplo {
+    /**
+     * Imprima todos os múltiplos de 3, entre 1 e 100.
+     */
+    public static void main(String[] args){
+        for (int i =1; i<100; i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
