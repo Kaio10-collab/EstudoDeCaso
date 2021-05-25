a3c97a57afd7ba32986fe779d9ab7986b5c41172
Kaio <kaio.moraes@zup.com.br>
2021-05-25 13:03:53 -0300

    Imprimir soma 1 até 1000.

--- teste1/src/com/br/zup/IfElse.java
+++ teste1/src/com/br/zup/IfElse.java
@@ -1,7 +1,9 @@
 package com.br.zup;
 
 public class IfElse {
-
+    /**
+     * Imprima todos os números de 150 a 300.
+     */
     public static void main(String[] args){
         for (int i = 150; i<=300; i++) {
             System.out.println(i);
