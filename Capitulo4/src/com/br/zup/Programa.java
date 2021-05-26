8b4e9ca57f5c139a2c865809be633d0edf09a673
Kaio <kaio.moraes@zup.com.br>
2021-05-26 14:12:05 -0300

    adicionar comentários.

--- Capitulo4/src/com/br/zup/Programa.java
+++ Capitulo4/src/com/br/zup/Programa.java
@@ -4,8 +4,15 @@ public class Programa {
 
     public static void main(String[] args) {
         // Estou criando a conta
-        Conta minhaConta;
-        minhaConta = new Conta();
+        Conta minhaConta; // minhaConta é uma variável referência.
+        minhaConta = new Conta(); // Estamos dizemos que a minhaConta é uma referência ao objeto Conta.
+/**
+ * LEMBRANDO QUE A VARIÁVEL NUNCA É UM OBJETO.
+ */
+        Conta meuSonho;
+        meuSonho = new Conta(); // new, depois de alocar a memória para esse objeto, devolve uma "flecha", isto é,
+        // um valor de referência. Quando você atribui isso a uma variável, essa variável passa a se referir para esse mesmo objeto.
+        meuSonho.saldo = 150000.0;
 
         // alterar os valores de minhaConta
         minhaConta.titular = "Kaio";
