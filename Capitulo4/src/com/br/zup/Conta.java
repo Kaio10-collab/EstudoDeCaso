050ddb276c7a03d0dc1347d14e66bd86315dbae5
Kaio <kaio.moraes@zup.com.br>
2021-05-26 12:42:48 -0300

    Adicionar saca e deposita saldo.

--- Capitulo4/src/com/br/zup/Conta.java
+++ Capitulo4/src/com/br/zup/Conta.java
@@ -1,7 +1,20 @@
 package com.br.zup;
 
 public class Conta {
+
+    // definição de atributos
     int numero;
     String titular;
     double saldo;
+    double salario;
+
+    //variável comum(também chamada de temporária), pois ao final da execução deste metódo, ela deixa de existir.
+    void sacaSaldo(double quantidade){
+        double novoSaldo = this.saldo - quantidade; // this mostra que é um atributo e não a variável.
+        this.saldo = novoSaldo;
+    }
+
+    void depositaSaldo(double quantidade) {
+        this.saldo += quantidade;
+    }
 }
--- Capitulo4/src/com/br/zup/Programa.java
+++ Capitulo4/src/com/br/zup/Programa.java
@@ -3,12 +3,22 @@ package com.br.zup;
 public class Programa {
 
     public static void main(String[] args) {
+        // Estou criando a conta
         Conta minhaConta;
         minhaConta = new Conta();
 
+        // alterar os valores de minhaConta
         minhaConta.titular = "Kaio";
         minhaConta.saldo = 1000.0;
 
-        System.out.println("Saldo atual" + minhaConta.saldo);
+        System.out.println("Saldo atual " + minhaConta.saldo);
+
+        // sacar valor do saldo
+        minhaConta.sacaSaldo(200);
+
+        // deposistando valor no sald
+        minhaConta.depositaSaldo(500);
+        System.out.println(minhaConta.saldo);
     }
+
 }
