c25bef2e6d04dee5a067491b5a681689fd2e7b18
Kaio <kaio.moraes@zup.com.br>
2021-05-27 18:16:10 -0300

    Criar classe Conta e seus atributos, getters e setters.

---
+++ Capitulo5/src/com/br/zup/Conta.java
@@ -0,0 +1,33 @@
+package com.br.zup;
+
+public class Conta {
+    //definição de atributos.
+    private String titular;
+    private double saldo;
+    private double limite;
+
+    //Criando getter e setters
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+}
