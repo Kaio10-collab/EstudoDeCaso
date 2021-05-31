35c490c1308e7915b16d9e02aefc66c032821bd2
Kaio <kaio.moraes@zup.com.br>
2021-05-31 17:03:04 -0300

    Explicar sobre a diferença do static e final.

---
+++ Capitulo9/src/com/br/zup/TarefaTai.java
@@ -0,0 +1,14 @@
+package com.br.zup;
+
+public class TarefaTai {
+    /**
+     * O uso do modificador STATIC em um atributo de classe, por exemplo, torna esse atributo possível de ser inicializado
+     * em qualquer parte da classe, mas quando recebe um valor, ele é compartilhado com todos os objetos da classe.
+     * Isso significa que os atributos estáticos de uma classe são compartilhados por todas as instâncias dessa classe.
+     * O modificador FINAL por sua vez, torna o atributo da classe uma constante e, só pode ser inicializado uma única vez,
+     * ou diretamente na declaração da classe ou no método construtor. Quando um atributo recebe os modificadores de acesso
+     * STATIC e FINAL, sua inicialização apresenta uma alteração distinta, o atributo não poderá mais ser inicializado no método construtor,
+     * sendo possível sua inicialização apenas diretamente na declaração do atributo ou no bloco de inicialização estático.
+     * https://www.devmedia.com.br/modificadores-de-acesso-do-java/27065
+     */
+}
